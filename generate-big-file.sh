611b90c1514a5b114d2e0b30a41cb6bda457fd3e
Amos Wenger <amos@bearcove.net>
2023-11-29 12:10:25 +0100

    Compare performance

---
+++ generate-big-file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# dd if=/dev/urandom of=bigfile bs=1M count=4096
+dd if=/dev/urandom of=bigfile bs=1M count=128
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"hash/crc64"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	// don't count table generation time
+	table := crc64.MakeTable(0x9A6C9329AC4BC9B5)
+
+	start := time.Now()
+
+	// read file "bigfile" and calculate its CRC64 checksum
+	// using the polynomial represented by the Table.
+
+	f, err := os.Open("bigfile")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	hash := crc64.New(table)
+
+	if _, err := io.Copy(hash, f); err != nil {
+		log.Fatal(err)
+	}
+
+	sum := hash.Sum(nil)
+	elapsed := time.Since(start)
+
+	log.Printf("hex digest: %X", sum)
+	log.Printf("time elapsed: %.2fms", elapsed.Seconds()*1000)
+}
